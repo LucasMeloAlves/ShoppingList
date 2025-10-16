19eaab49515e377876ab7c966d2def2870b71032
lucasmeloalves <lucas.melo@edu.unifi.it>
2025-10-16 10:01:55 +0200

    google test

---
+++ test/runAllTests.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Lucas Melo Alves on 15/10/25.
+//
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+
+}
---
+++ test/shoppingListTest.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Lucas Melo Alves on 15/10/25.
+//
